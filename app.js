0e6b2e42dee2504a59eaad17819cf209869b7871
Saurabh Pathak <SPathak@eagle.org>
2023-01-14 17:51:33 +0530

    3.Error Hanlding

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app =  express();
+const morgan = require('morgan');
 
 const productRoutes = require('./Api/routes/products')
 const orderRoutes = require('./Api/routes/orders')
 
+// Logger for node js
+app.use(morgan('dev'))
+
+// Routes which should handle request
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+
+// Error Handling
+app.use((req,res,next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message : error.message
+        }
+    })
+})
 
 module.exports = app;
